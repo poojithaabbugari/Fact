50cc300a9e7784475638058b17d78b8f190ca1c5
ABBUGARI POOJITHA <poojithaabbugari041@gmail.com>
2025-08-22 22:11:03 +0530

    fact.py

--- fact.py
+++ fact.py
@@ -1,11 +1,12 @@
 
 n = 4 #taking input of integer type 4
 fact = 1 #taking variable fact is 1
-for i in range(1,n+1): #The Range Function consisting of values(1,2,3,4(n+1))
-    fact = fact * i #fact is 1 then (1 * 1 = 1), fact value is 1 and (1 * 2 = 2) fact value is 2(upgraded/modified) 3 = 6),here fact is 6(Upgraded/modified) 4 = 24),here fact is 24(upgraded/modified)
+for i in range(1,n+1): #The Range Function consisting of values(i = 1,2,3,4(n+1))
+    fact = fact * i #fact is 1 then (1 * 1 = 1), fact value is 1 and (1 * 2 = 2) fact value is 2(upgraded/modified) and (2 * 3 = 6),here fact is 6(Upgraded/modified) and (6 * 4 = 24),here fact is 24(upgraded/modified)
 print("Factorial of ", n,"is", fact) #print is a function it prints Factorial of 4 is 24
   
 
 
   #The output is
+
   #Factorial of 4 is 24
